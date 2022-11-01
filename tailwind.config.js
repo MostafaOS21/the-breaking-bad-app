6b9bee2d74435df9d3ffaffb8289983b06002466
Mostafa Osama <81161532+MostafaOS21@users.noreply.github.com>
2022-11-02 00:32:16 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,tsx}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
