8335fe23a1cac12b54ca33bef7dd43abf3db7534
Mostafa Osama <81161532+MostafaOS21@users.noreply.github.com>
2022-11-02 00:24:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// React Router
+import { Routes, Route, Link, useLocation } from "react-router-dom";
+
+// Recoil
+import { RecoilRoot } from "recoil";
+
+// Navbar
+import Navbar from "./components/Navbar";
+
+// Welcome
+import Welcome from "./components/Welcome";
+
+// Characters
+import Characters from "./components/Characters";
+
+// Single Character
+import SingleCharacter from "./components/SingleCharacter";
+
+// Episodes
+import Episodes from "./components/Episodes";
+// Single Episodes
+import SingleEpisode from "./components/SingleEpisode";
+import Quotes from "./components/Quotes";
+import Deaths from "./components/Deaths";
+
+// Framer Motion
+import {AnimatePresence} from 'framer-motion';
 
 function App() {
+  // See Dark theme
+
+  if(localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+  }
+
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="App relative dark:bg-slate-800 bg-white transition-all duration-700">
+        <Navbar/>
+        <AnimatePresence>
+
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/characters/:charId" element={<SingleCharacter />} />
+          <Route path="/episodes" element={<Episodes/>} />
+          <Route path="/episodes/:epsId" element={<SingleEpisode/>} />
+          <Route path="/quotes" element={<Quotes/>} />
+          <Route path="/deaths" element={<Deaths/>} />
+        </Routes>
+        </AnimatePresence>
+      </div>
+    </RecoilRoot>
   );
 }
 
